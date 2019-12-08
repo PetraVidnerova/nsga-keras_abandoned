653e1ac6bd867023cdd5a644e76c33fd9a7b1ddf
Petra Vidnerová <petra.vidnerova@gmail.com>
2019-12-08 20:08:51 +0100

    run on one card

--- run.sh
+++ run.sh
@@ -1,2 +1,8 @@
-python main.py --type conv --trainset mnist2d.train --testset mnist2d.test --id test 1> test.log 2> err.log 
+export CUDA_VISIBLE_DEVICES=0 
+python main.py --type conv --trainset mnist2d.train --testset mnist2d.test --id haklnv_conv 1> test_haklnv_conv.log 2> err_haklnv_conv.log 
 
+# for I in `seq 1 4`
+# do
+#     export CUDA_VISIBLE_DEVICES=$I 
+#     python main.py --type conv --trainset mnist2d.train --testset mnist2d.test --id haklnv_conv_$I 1> test_haklnv_conv_$I.log 2> err_haklnv_conv_$I.log &
+# done 
